bc13c2076451cf652d0662d439487ed36b33c6f3
Shane Keney <shane@spikeball.com>
2022-06-27 15:47:57 -0400

    feat: send emails via contact route

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,29 @@
 import express, { NextFunction, Request, Response } from 'express'
+import sgMail from "@sendgrid/mail"
+import { json } from 'body-parser'
 
 const routes = (app: express.Application) => {
 
-  app.post('/api/contact', (req, res) => {
+  app.post('/api/contact', async (req, res) => {
     // TODO: Send email to personal account based on req body
     console.log(req.body)
-    return "hello"
+    const email = {
+      to: process.env.CONTACT_EMAIL,
+      from: req.body.email,
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    sgMail
+      .send(email)
+      .then(() => {
+        return res.status(201).json({ message: 'Email sent successfully' })
+      })
+      .catch(error => res.status(500).json({ 
+        errorCode: 'INTERNAL_SERVER_ERROR', 
+        message:  'Error sending email to Shane Keney.  Please try again later.'
+      }))
   })
 
   /**
